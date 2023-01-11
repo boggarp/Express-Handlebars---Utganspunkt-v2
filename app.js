dcb187c7e425b9dd71e4b258448d134cacf96195
boggarp <borge.garpestad@vlfk.no>
2023-01-11 15:34:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = require('./setup.js').app
+
+//Handler fremsiden
+
+//Handler about-siden
+
+//Handler for Innput - skjema
+
+//Starter opp applikasjonen
+app.listen(3000, function() { 
+    console.log("Server is up! Check http://localhost:3000")
+})
