6520570dbb9778eb7d2700c41d564f92f550f5bd
boggarp <borge.garpestad@vlfk.no>
2023-01-11 15:37:57 +0100

    view old and new way of handler coding

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const app = require('./setup.js').app
 
 //Handler fremsiden
+//Old way:
+//function hovedSideRute(request, response){
+//    response.render("index.hbs")
+//}
+//app.get('',hovedSideRute)
+
+//New way:
+app.get('', function (request,response) {
+    response.render("index.hbs")
+})
 
 //Handler about-siden
 
